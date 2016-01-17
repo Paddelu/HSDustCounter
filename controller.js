a06ad210009a50605a429ea249bfb88b2498fd58
Paddelu <patrik.paarnio@gmail.com>
2016-01-18 01:22:14 +0200

    one function for all

--- controller.js
+++ controller.js
@@ -1,19 +1,7 @@
 var app = angular.module('myApp', []);
 
 app.controller('MainCtrl', function($scope) {
-$scope.addC=function(rarity){
-    $scope.common = parseInt(rarity)+1;
-}
-
-$scope.addR=function(rarity){
-    $scope.rare = parseInt(rarity)+1;
-}
-
-$scope.addE=function(rarity){
-    $scope.epic = parseInt(rarity)+1;
-}
-
-$scope.addL=function(rarity){
-    $scope.legendary = parseInt(rarity)+1;
+$scope.add=function(rarity,amount){
+    $scope[rarity] = parseInt(amount) + 1;
 }
 });
