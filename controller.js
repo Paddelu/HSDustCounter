2e788945482f5ebef35ade468a6ca90139bceef3
Paddelu <patrik.paarnio@gmail.com>
2016-02-12 11:05:41 +0200

    fixed angular error

--- controller.js
+++ controller.js
@@ -4,4 +4,9 @@ app.controller('MainCtrl', function($scope) {
 $scope.add=function(rarity,amount){
     $scope[rarity] = parseInt(amount) + 1;
 }
+$scope.DustAmount = [{name:'Common',cost: 5}, {name:'Rare',cost: 20}, {name:'Epic',cost: 100}, {name:'Legendary',cost: 400}];
+$scope.common = null;
+$scope.rare= null;
+$scope.epic = null ;
+$scope.legendary = null;
 });
