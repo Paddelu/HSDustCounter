b65cbb5456526aefed67ea5107088d1560644669
Paddelu <patrik.paarnio@gmail.com>
2016-02-12 14:50:45 +0200

    fixed the null error

---
+++ dustCounter.js
@@ -0,0 +1,11 @@
+app.controller('MainCtrl',function($scope) {
+$scope.add=function(rarity,amount){
+    $scope[rarity] = parseInt(amount) || 0;
+    $scope[rarity]++;
+}
+$scope.DustAmount = [{name:'Common',cost: 5}, {name:'Rare',cost: 20}, {name:'Epic',cost: 100}, {name:'Legendary',cost: 400}];
+$scope.common = null;
+$scope.rare= null;
+$scope.epic = null ;
+$scope.legendary = null;
+});
