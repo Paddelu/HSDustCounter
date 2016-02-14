09e0d084d19d715c672f6c32619842aad0d03295
Paddelu <patrik.paarnio@gmail.com>
2016-02-14 22:07:13 +0200

    implemented views

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-var app = angular.module('myApp', []);
+var app = angular.module('myApp', ['ngRoute']);
+
+    // configure our routes
+    app.config(function($routeProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'home.html',
+                controller  : 'HomeCtrl'
+            })
+        
+            // route for the home page
+            .when('/counter', {
+                templateUrl : 'counter.html',
+                controller  : 'MainCtrl'
+            })
+
+            // route for the about page
+            .when('/cards', {
+                templateUrl : 'cards.html',
+                controller  : 'JsonCtrl'
+            });
+    });
