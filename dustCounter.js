863d58db47fb77369bedb285b8928b36664b39b2
Paddelu <patrik.paarnio@gmail.com>
2016-02-21 18:12:18 +0200

    own fields and fixed substraction

--- dustCounter.js
+++ dustCounter.js
@@ -1,28 +1,20 @@
 app.controller('MainCtrl',function($scope) {
-    
         $scope.adds=function(rarity,amount){
-        $scope[rarity] = parseInt(amount) || 0;
-        $scope[rarity]++;
-    }
-        
-        $scope.add=function(rarity,amount,g,position){
-            $scope.gt += $scope.DustAmount[position].cost * $scope.DustAmount[position].golden;
-            console.log($scope.gt);
-            $scope[rarity] == parseInt(amount) || 0;
+            $scope[rarity] = parseInt(amount) || 0;
             $scope[rarity]++;
-            $scope[g] += $scope.DustAmount[position].golden * $scope.DustAmount[position].cost - $scope.DustAmount[position].cost;
-            console.log($scope[g]);
-            $scope.gt -= $scope.DustAmount[position].cost;
-    }
-        
+            $scope.ct = $scope.DustAmount[0].cost * $scope.common + $scope.DustAmount[0].cost * $scope.Gcommon * $scope.DustAmount[0].golden;
+            $scope.rt = $scope.DustAmount[1].cost * $scope.rare + $scope.DustAmount[1].cost * $scope.Grare * $scope.DustAmount[1].golden;
+            $scope.et = $scope.DustAmount[2].cost * $scope.epic + $scope.DustAmount[2].cost * $scope.Gepic * $scope.DustAmount[2].golden;
+            $scope.lt = $scope.DustAmount[3].cost * $scope.legendary + $scope.DustAmount[3].cost * $scope.Glegendary * $scope.DustAmount[3].golden;
+            $scope.tt = $scope.ct + $scope.rt + $scope.et + $scope.lt;
+    } 
     $scope.DustAmount = [{name:'Common',cost: 5, golden: 10}, {name:'Rare',cost: 20, golden: 5}, {name:'Epic',cost: 100, golden: 4}, {name:'Legendary',cost: 400, golden: 4}];
     $scope.common = null;
     $scope.rare= null;
     $scope.epic = null;
     $scope.legendary = null;
-    $scope.gc = 0;
-    $scope.gr = 0;
-    $scope.ge = 0;
-    $scope.gl = 0;
-    $scope.gt = 0;
+    $scope.Gcommon = null;
+    $scope.Grare= null;
+    $scope.Gepic = null;
+    $scope.Glegendary = null;
 });
