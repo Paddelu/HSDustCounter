55a788fcbaa4ea6e61f3d4f036d7c23bfefd13ce
Paddelu <patrik.paarnio@gmail.com>
2016-02-21 19:13:44 +0200

    rewrote code

--- dustCounter.js
+++ dustCounter.js
@@ -1,11 +1,9 @@
 app.controller('MainCtrl',function($scope) {
-        $scope.adds=function(rarity,amount){
-            $scope[rarity] = parseInt(amount) || 0;
-            $scope[rarity]++;
-            $scope.ct = $scope.DustAmount[0].cost * $scope.common + $scope.DustAmount[0].cost * $scope.Gcommon * $scope.DustAmount[0].golden;
-            $scope.rt = $scope.DustAmount[1].cost * $scope.rare + $scope.DustAmount[1].cost * $scope.Grare * $scope.DustAmount[1].golden;
-            $scope.et = $scope.DustAmount[2].cost * $scope.epic + $scope.DustAmount[2].cost * $scope.Gepic * $scope.DustAmount[2].golden;
-            $scope.lt = $scope.DustAmount[3].cost * $scope.legendary + $scope.DustAmount[3].cost * $scope.Glegendary * $scope.DustAmount[3].golden;
+        $scope.adds=function(rarity,amount,field,pos,prefix){
+
+            $scope[prefix+rarity] = parseInt(amount) || 0;
+            $scope[prefix+rarity]++;
+            $scope[field] = $scope.DustAmount[pos].cost * $scope[rarity] + $scope.DustAmount[pos].cost * $scope.DustAmount[pos].golden * $scope["G"+rarity];
             $scope.tt = $scope.ct + $scope.rt + $scope.et + $scope.lt;
     } 
     $scope.DustAmount = [{name:'Common',cost: 5, golden: 10}, {name:'Rare',cost: 20, golden: 5}, {name:'Epic',cost: 100, golden: 4}, {name:'Legendary',cost: 400, golden: 4}];
@@ -17,4 +15,9 @@ app.controller('MainCtrl',function($scope) {
     $scope.Grare= null;
     $scope.Gepic = null;
     $scope.Glegendary = null;
+    $scope.ct = null;
+    $scope.rt = null;
+    $scope.et = null;
+    $scope.lt = null;
+    $scope.tt = null;
 });
